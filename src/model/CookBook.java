3dc0a5518bde5344d8bdbec22f9b6a8eea765dad
mk0131 <jmg21128@gmail.com>
2022-06-01 16:50:58 +0900

    COMMIT EXP

---
+++ src/model/CookBook.java
@@ -0,0 +1,28 @@
+package model;
+
+public class CookBook extends Book {
+    private boolean coupon;        // 요리학원 쿠폰 유무
+
+    public CookBook() {
+
+    }
+
+    public CookBook(String title, String author, String publisher, boolean coupon) {
+        super(title, author, publisher);
+        this.coupon = coupon;
+    }
+
+
+    public boolean isCoupon() {
+        return coupon;
+    }
+
+    public void setCoupon(boolean coupon) {
+        this.coupon = coupon;
+    }
+
+    @Override
+    public String toString() {
+        return "CookBook [" + super.toString() + ", coupon=" + coupon + "]";
+    }
+}
---
+++ src/run/Run.java
@@ -0,0 +1,9 @@
+package run;
+
+import view.LibraryMenu;
+
+public class Run {
+    public static void main(String[] args) {
+        new LibraryMenu().mainMenu();
+    }
+}
